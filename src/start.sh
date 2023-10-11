37f66d04b8c98810714ffbc761412f3fcdb1d861
TimPietrusky <timpietrusky@gmail.com>
2023-10-11 11:47:57 +0200

    fix: updated path to "comfyui"

--- src/start.sh
+++ src/start.sh
@@ -5,7 +5,7 @@ TCMALLOC="$(ldconfig -p | grep -Po "libtcmalloc.so.\d" | head -n 1)"
 export LD_PRELOAD="${TCMALLOC}"
 
 echo "runpod-worker-comfy: Starting ComfyUI"
-python3 /ComfyUI/main.py --disable-auto-launch --disable-metadata &
+python3 /comfyui/main.py --disable-auto-launch --disable-metadata &
 
 echo "runpod-worker-comfy: Starting RunPod Handler"
 python3 -u /rp_handler.py
