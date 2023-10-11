fc731fffcd79af67cf6fcdf6a6d3df6b8e30c7b5
TimPietrusky <timpietrusky@gmail.com>
2023-10-11 11:26:52 +0200

    feat: logs should be written to stdout so that we can see them inside the worker

--- src/start.sh
+++ src/start.sh
@@ -1,12 +1,11 @@
 #!/usr/bin/env bash
 
-echo "runpod-worker-comfy: Starting ComfyUI"
-
 # Use libtcmalloc for better memory management
 TCMALLOC="$(ldconfig -p | grep -Po "libtcmalloc.so.\d" | head -n 1)"
 export LD_PRELOAD="${TCMALLOC}"
 
-python /ComfyUI/main.py --disable-auto-launch --disable-metadata > /workspace/logs/comfy.log 2>&1 &
+echo "runpod-worker-comfy: Starting ComfyUI"
+python3 /ComfyUI/main.py --disable-auto-launch --disable-metadata &
 
 echo "runpod-worker-comfy: Starting RunPod Handler"
 python3 -u /rp_handler.py
