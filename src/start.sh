fb17089ca6e98b0654e628d2c62731f23383256e
Tim Pietrusky <timpietrusky@gmail.com>
2024-02-11 23:16:18 +0100

    feat: the local API should run on 0.0.0.0

--- src/start.sh
+++ src/start.sh
@@ -11,7 +11,7 @@ echo "runpod-worker-comfy: Starting RunPod Handler"
 
 # Serve the API and don't shutdown the container
 if [ "$SERVE_API_LOCALLY" == "true" ]; then
-    python3 -u /rp_handler.py --rp_serve_api
+    python3 -u /rp_handler.py --rp_serve_api --rp_api_host=0.0.0.0
 else
     python3 -u /rp_handler.py
 fi
