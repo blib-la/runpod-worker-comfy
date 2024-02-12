34eb32b72455e6e628849e50405ed172d846d2d9
NERDDISCO <492378+TimPietrusky@users.noreply.github.com>
2024-02-12 16:43:22 +0100

    feat: run the worker locally (#19)
    * feat: provide option to run the handler locally as API
    
    * ci: run the workflow on our extended instance
    
    * feat: the local API should run on 0.0.0.0
    
    * ci: use semantic-release to create releases automatically

--- src/start.sh
+++ src/start.sh
@@ -8,4 +8,10 @@ echo "runpod-worker-comfy: Starting ComfyUI"
 python3 /comfyui/main.py --disable-auto-launch --disable-metadata &
 
 echo "runpod-worker-comfy: Starting RunPod Handler"
-python3 -u /rp_handler.py
+
+# Serve the API and don't shutdown the container
+if [ "$SERVE_API_LOCALLY" == "true" ]; then
+    python3 -u /rp_handler.py --rp_serve_api --rp_api_host=0.0.0.0
+else
+    python3 -u /rp_handler.py
+fi
