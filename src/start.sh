201f64ebbadf34fddb209b395a55b19342e7ccdb
Tim Pietrusky <timpietrusky@gmail.com>
2024-03-19 09:56:06 +0100

    feat: provide access to ComfyUI via web

--- src/start.sh
+++ src/start.sh
@@ -4,14 +4,17 @@
 TCMALLOC="$(ldconfig -p | grep -Po "libtcmalloc.so.\d" | head -n 1)"
 export LD_PRELOAD="${TCMALLOC}"
 
-echo "runpod-worker-comfy: Starting ComfyUI"
-python3 /comfyui/main.py --disable-auto-launch --disable-metadata &
-
-echo "runpod-worker-comfy: Starting RunPod Handler"
-
 # Serve the API and don't shutdown the container
 if [ "$SERVE_API_LOCALLY" == "true" ]; then
+    echo "runpod-worker-comfy: Starting ComfyUI"
+    python3 /comfyui/main.py --disable-auto-launch --disable-metadata --listen &
+
+    echo "runpod-worker-comfy: Starting RunPod Handler"
     python3 -u /rp_handler.py --rp_serve_api --rp_api_host=0.0.0.0
 else
+    echo "runpod-worker-comfy: Starting ComfyUI"
+    python3 /comfyui/main.py --disable-auto-launch --disable-metadata &
+
+    echo "runpod-worker-comfy: Starting RunPod Handler"
     python3 -u /rp_handler.py
 fi
