4b48ef0f1abb67e70a088ff87d936f91deb1b159
Tim Pietrusky <timpietrusky@gmail.com>
2024-03-19 12:15:19 +0100

    fix: use the full path to the output image

--- src/rp_handler.py
+++ src/rp_handler.py
@@ -237,7 +237,7 @@ def process_output_images(outputs, job_id):
     for node_id, node_output in outputs.items():
         if "images" in node_output:
             for image in node_output["images"]:
-                output_images = image["filename"]
+                output_images = os.path.join(image["subfolder"], image["filename"])
 
     print(f"runpod-worker-comfy - image generation is done")
 
